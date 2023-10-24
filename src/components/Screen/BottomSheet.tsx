806c943b49dfb50375fcca07faab15cc56eac165
SeobisBack <seobisback@gmail.com>
2023-10-24 10:28:31 +0900

    Update BottomSheet.tsx

--- src/components/Screen/BottomSheet.tsx
+++ src/components/Screen/BottomSheet.tsx
@@ -28,6 +28,7 @@ const BottomSheet = () => {
             // 만약 translateY 값이 SCREEN_HEIGHT 를 3으로 나눈 값보다 작을 경우 0으로 만들어 바텀 시트 종료
             scrollTo(0)
         } else if(translateY.value < -SCREEN_HEIGHT / 1.5){
+            // 만약 translateY 값이 SCREEN_HEIGHT 를 1.5으로 나눈 값보다 클 경우 MAX_TRANSLATE_Y(최대값)으로 설정함
             scrollTo(MAX_TRANSLATE_Y)
         }
     });
